0fdc9edb993e61385cc8e5590f8fbbfb387615f4
CameronMclean1 <97161067+CameronMclean1@users.noreply.github.com>
2022-01-20 12:30:32 +0000

    Add files via upload

--- api_tests.py
+++ api_tests.py
@@ -9,7 +9,7 @@ class UnitTests:
     def __init__(self):
         print('initialising')
         self.Luke = Pilot("https://swapi.dev/api/people/1/")
-        self.Falcon = Ship(requests.get('https://swapi.dev/api//starships/10/').json())
+        self.VaderShip = Ship(requests.get('https://swapi.dev/api//starships/13/').json())
 
         self.check_pilots()
         self.check_starships()
@@ -22,24 +22,24 @@ class UnitTests:
         return
 
     def check_starships(self):
-        falcon_pilots = []
-        for pilot in self.Falcon.starship['pilots']:
+        VaderShip_pilots = []
+        for pilot in self.VaderShip.starship['pilots']:
             pilot = Pilot(pilot)
-            falcon_pilots.append(pilot.pilot['name'])
-        if 'Han Solo' in falcon_pilots:
-            print("Ships class correctly identified Han Solo as a pilot on the Millenium Falcon")
+            VaderShip_pilots.append(pilot.pilot['name'])
+        if 'Darth Vader' in VaderShip_pilots:
+            print("Ships class correctly identified Darth Vader as a pilot")
         else:
-            print("Ships class failed to identify Han Solo as a pilot on the Millenium Falcon")
-        self.Falcon.alter_pilots()
+            print("Ships class failed to identify Darth Vader as a pilot")
+        self.VaderShip.alter_pilots()
 
-        falcon_pilots = self.Falcon.starship['pilots']
+        VaderShip_pilots = self.VaderShip.starship['pilots']
         i=0
         while True:
-            if i>=len(falcon_pilots):
-                print("Han Solo not correctly turned into MongoDB id")
+            if i>=len(VaderShip_pilots):
+                print("Darth Vader not correctly turned into MongoDB id")
                 break
-            elif "61e58b896c7bb37eaf8aba4c" in str(falcon_pilots[i]):
-                print("Han Solo correctly turned into MongoDB id")
+            elif "61e58b8140d779a8d6e04967" in str(VaderShip_pilots[i]):
+                print("Darth Vader correctly turned into MongoDB id")
                 break
             else:
                 i+=1
